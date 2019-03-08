da586af1386ab05eaa3320b01265235ba98ce3f7
LoganElliss <47399984+LoganElliss@users.noreply.github.com>
2019-03-08 14:40:45 -0500

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _011
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            System.IO.StreamReader reader = new System.IO.StreamReader("My File.txt");
+            try {
+                string output = "";
+                while (!reader.EndOfStream)
+                {
+                    output += reader.ReadLine();
+                   +Environment.NewLine;
+                }
+                output = output.Trim(;)
+                lblOutput.Content = output;
+
+            } catch (Exception ex) { }
+        }
+}   }
